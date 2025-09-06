874ff4706747ac925249f4ffe1baafcbed8fc57d
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-09-06 21:42:43 +0545

    client and server components

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import AddtoCart from "./AddtoCart";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddtoCart />
+    </div>
+  );
+};
+
+export default ProductCard;
