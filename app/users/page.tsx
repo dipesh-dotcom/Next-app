336134ffb843f76f659cb957d9eb70b42598d27e
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-09-07 14:05:11 +0545

    data fetching and caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>this is user page</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UserPage;
